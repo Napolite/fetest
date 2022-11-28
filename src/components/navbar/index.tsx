dbeea0402b2ce0cf65611ddee7e6ed0192a4f498
Constantino <Okonemmanuelmma@gmail.com>
2022-11-28 15:57:53 +0100

    navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,41 +1,41 @@
 import React from "react";
-import Logo from "../../assets/images/Group (1).png";
-import NotificationIcon from "../../assets/images/notifications.png";
-import avatar from "../../assets/images/no image.png";
+import NotificationIcon from "../../assets/images/notification.png";
+import avatar from "../../assets/images/avatar.png";
+import search from "../../assets/images/Search.png";
 import "./navbar.css";
 
 function NavBar({ active }: any) {
   return (
     <div className="navbar-root">
       <div>
-        <img src={Logo} alt="" className="nav-logo" />
+        <div className="nav-logo">TransMonitor</div>
       </div>
       <div className="nav-menu-parent">
         <div className="nav-menu">
-          <div>Dashboard</div>
-
-          <div>Tracking</div>
-
-          <div>Users</div>
-
-          <div>Drivers&Fleets</div>
-
-          <div> Payments</div>
-          <div>Promotions</div>
-        </div>
-      </div>
-      <div className="accounts">
-        <div>
-          <img src={NotificationIcon} alt="" />
-        </div>
-        <div className="avatar-details">
-          <img src={avatar} alt="" />
           <div>
-            <div>Obasi Adekunle</div>
-            <div>Super Admin</div>
+            <label>
+              <img src={search} />
+            </label>
+            <input placeholder="Search..." disabled />
           </div>
         </div>
-        <div className="arrow-down"></div>
+      </div>
+      <div className="left-side">
+        <div>Support</div>
+        <div>FAQ</div>
+        <div className="accounts">
+          <div>
+            <img src={NotificationIcon} alt="" />
+          </div>
+          <div className="avatar-details">
+            <img src={avatar} alt="" />
+            <div>
+              <div>Obasi Adekunle</div>
+              <div>Super Admin</div>
+            </div>
+          </div>
+          <div className="arrow-down"></div>
+        </div>
       </div>
     </div>
   );
